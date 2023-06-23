da8a2f7c0fa93ee225c405ce772c425aa2f9f564
azmrfii <azzamrafizafran@gmail.com>
2023-06-23 14:18:06 +0700

    first commit

---
+++ app/Http/Controllers/Frontend/WelcomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Menu;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        $menus = Menu::all();
+        return view('welcome', compact('menus'));
+    }
+
+
+
+    public function thankyou()
+    {
+        return view('thankyou');
+    }
+}
